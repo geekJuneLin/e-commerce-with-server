b0a21317d555a1587f0a71456643b900acb700ff
geekJuneLin <junelin0216@gmail.com>
2020-04-27 16:05:07 +1200

    added the built dir

---
+++ app/build/precache-manifest.2b9f9145d81f615bb3263d4a67f7a6c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cb267f511c5b3f5bdff8d8b123219b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2568f3de62ffc7dad123",
+    "url": "/static/css/2.3067220f.chunk.css"
+  },
+  {
+    "revision": "1fe619a1f3eafc634a20",
+    "url": "/static/css/main.47638d25.chunk.css"
+  },
+  {
+    "revision": "2568f3de62ffc7dad123",
+    "url": "/static/js/2.d3fb548f.chunk.js"
+  },
+  {
+    "revision": "e8aff7ec78d6629d1410767c2b0daf4d",
+    "url": "/static/js/2.d3fb548f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fe619a1f3eafc634a20",
+    "url": "/static/js/main.69bbd42d.chunk.js"
+  },
+  {
+    "revision": "2c503c0971e123f4712b",
+    "url": "/static/js/runtime-main.e813a1e1.js"
+  }
+]);
